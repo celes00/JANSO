2fc661109ce1e1f4a1d4e20af8df73d063b7ff26
Maria Celeste Antelo <103297382+celes00@users.noreply.github.com>
2023-07-20 11:51:13 -0300

    agrego seccion our service

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <?php /* para el envio del formulario */
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
+/* if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $name = $_POST["name"];
   $email = $_POST["email"];
   $company = $_POST["company"];
@@ -10,12 +10,58 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $subject = "Consulta de la página web - " . $name . " - " . $company;
   $body = "Nombre: " . $name . "\n" . "Empresa: " . $company . "\n" . "Correo electrónico: " . $email . "\n\n" . "Mensaje:\n" . $message;
 
-  // Envía el correo electrónico
+  // Envía el correo electrónico 
   $headers = "From: " . $email . "\r\n";
   if (mail($to, $subject, $body, $headers)) {
     echo "Mensaje enviado correctamente.";
   } else {
     echo "Hubo un error al enviar el mensaje.";
   }
-}
+} */
+  /* $name = $_POST["name"];
+  $email = $_POST["email"];
+  $company = $_POST["company"];
+  $message = $_POST["message"];
+
+  echo $name . " " . $email . " ". $company . " " . $message; */
+
+
+  $correo = $_POST['correo'];
+  $nombre = $_POST['nombre'];
+  $mensaje = $_POST['mensaje'];
+  
+  
+echo $correo . " " . $nombre . " " . $mensaje;
+  
+  
+  $destinatario = "mariacelesteantelo@gmail.com";
+  $asunto = "Envio de correo de prueba con PHP"; 
+  $cuerpo = '
+      <html> 
+          <head> 
+              <title>Prueba de envio de correo</title> 
+          </head>
+  
+          <body> 
+              <h1>Solicitud de contacto desde correo de prueba !  </h1>
+              <p> 
+                  Contacto:  '.$nombre . ' - ' . $asunto .'  <br>
+                  Mensaje: '.$mensaje.' 
+              </p> 
+          </body>
+      </html>
+  ';
+  //para el envío en formato HTML 
+  $headers = "MIME-Version: 1.0\r\n"; 
+  $headers .= "Content-type: text/html; charset=UTF8\r\n"; 
+  
+  //dirección del remitente
+  
+  $headers .= "FROM: $nombre <$correo>\r\n";
+  mail($destinatario,$asunto,$cuerpo,$headers);
+  
+  echo "Correo enviado"; 
+  ?> 
+  
+  <a href="index.html">Volver a inicio</a>
 ?>
